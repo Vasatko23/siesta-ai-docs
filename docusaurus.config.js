1290609848e83120a364fd2c8cb08e94f7df27cd
Ondřej Vašátko <ondrejvasatko@Ondrej--MacBook-Air.local>
2025-07-06 14:32:13 +0200

    Fix baseUrl for Vercel deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://vasatko23.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/siesta-ai-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
