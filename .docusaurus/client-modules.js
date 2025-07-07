e5a2622a5a84c9d42338e271ebc96b718035cebb
Ondřej Vašátko <ondrejvasatko@Ondrej--MacBook-Air.local>
2025-07-07 11:11:57 +0200

    Deploy website - based on 6d7650cf2f8789dc5f7a4b4404cf61f806551432

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/ondrejvasatko/Desktop/my-docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/ondrejvasatko/Desktop/my-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/ondrejvasatko/Desktop/my-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/ondrejvasatko/Desktop/my-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/ondrejvasatko/Desktop/my-docs/src/css/custom.css"),
-];
